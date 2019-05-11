aa0e68cc5cc5c826ce8228fd10c1d6ac6af2eabe
be-doing <1782904648@qq.com>
2019-05-11 22:43:29 +0800

    c++双向链表

--- 双向循环链表/双向循环链表/DuList.h
+++ 双向循环链表/双向循环链表/DuList.h
@@ -10,37 +10,65 @@ public:
 		:val_(val)
 		, next_(nullptr)
 		, pre_(nullptr)
-	{
-		//DuLListNode<T>* node = new DuLListNode<T>;
-		//next_ = this;
-		//pre_ = this;
-	}
-	~DuLListNode()
-	{
-
-	}
+	{}
 	T val_;
 	DuLListNode<T>* next_;
 	DuLListNode<T>* pre_;
 };
+
 template<class T>
 class DList
 {
+	typedef DuLListNode<T> Node;
 	typedef DuLListNode<T>* PNode;
 public:
-	DList(const T& val = T())
+	DList()
 	{
-		PNode head_ = new  DuLListNode<T>;
+		head_ = new  Node;
 		head_->next_ = head_;
 		head_->pre_ = head_;
 	}
 	void Push_Back(const T& val)
 	{
-		PNode node(val);
+		PNode node = new Node(val);
 		node->next_ = head_;
 		node->pre_ = head_->pre_;
 		head_->pre_->next_ = node;
 		head_->pre_ = node;
+	}
+	void Pop_Back()
+	{
+		if (head_)
+		{
+			head_->pre_->pre_->next_ = head_;
+			head_->pre_ = head_->pre_->pre_;
+		}
+	}
+	void Push_Front(const T& val)
+	{
+		PNode node = new Node(val);
+		node->next_ = head_->next_;
+		node->pre_ = head_;
+		head_->next_->pre_ = node;
+		head_->next_ = node;
+	}
+	void Pop_Front()
+	{
+		if (head_)
+		{
+			head_->next_->next_->pre_ = head_;
+			head_->next_ = head_->next_->next_;
+		}
+	}
+	void Print()
+	{
+		PNode tmp = head_->next_;
+		while (tmp != head_)
+		{
+			cout << tmp->val_ << " ";
+			tmp = tmp->next_;
+		}
+		cout << endl;
 	}
 	~DList()
 	{
@@ -61,8 +89,4 @@ public:
 private:
 	PNode head_;
 };
-template<class T>
-void Print(DList<T>& ls)
-{
 
-}
--- 双向循环链表/双向循环链表/源.cpp
+++ 双向循环链表/双向循环链表/源.cpp
@@ -3,10 +3,34 @@
 void practice1()
 {
 	DList<int> ls;
-
 	ls.Push_Back(1);
+	ls.Push_Back(2);
+	ls.Push_Back(3);
+	ls.Push_Back(4);
+	ls.Print();
+
+	ls.Pop_Back();
+	ls.Pop_Back();
+	ls.Print();
+}
+void practice2()
+{
+	DList<int> ls;
+
+	ls.Push_Front(4);
+	ls.Push_Front(3);
+	ls.Push_Front(2);
+	ls.Push_Front(1);
+	ls.Print();
+
+	ls.Pop_Front();
+	ls.Pop_Front();
+	ls.Pop_Front();
+	ls.Print();
 }
 int main()
 {
+	practice1();
+	system("pause");
 	return 0;
 }
